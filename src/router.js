1202a9759a4c7635470afe5834c5311592f9565c
Toshihiro Yokora <yokoto@Toshihiro-no-MacBook-Pro.local>
2019-08-04 15:13:31 +0900

    first commit

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// ルート用のコンポーネントを読み込む
+import Home from '@/views/Home.vue'
+import Product from '@/views/Product.vue'
+// Vuexと同様で最初にプラグインとして登録
+Vue.use(VueRouter)
+
+// VueRouterインスタンスを生成する
+const router = new VueRouter({
+  // URLのパスと紐づくコンポーネントをマッピング
+  routes: [
+    { path: '/', component: Home },
+    { path: '/product', component: Product }
+  ]
+})
+
+// 生成したVueRouterインスタンスをエクスポート
+export default router
