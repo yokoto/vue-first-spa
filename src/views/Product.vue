5927a1602a71feb1a473c9d9892e7a874ba8fbf5
Toshihiro Yokora <yokoto@Toshihiro-no-MacBook-Pro.local>
2019-08-06 23:48:27 +0900

    【WIP】Add vuex and parent route, Product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="product" v-if="item" key="product">
-    <h1>商品情報</h1>
-    <dl class="product-table">
-      <dt>商品名</dt><dd>{{ item.name }}</dd>
-      <dt>価格</dt><dd>{{ item.price }}円</dd>
-      <dt>商品説明</dt><dd>{{ item.content }}</dd>
-    </dl>
+  <div class="product">
+    <h1>{{ detail.name }}</h1>
+    <nav class="nav">
+      <router-link :to="{ name: 'product-home' }">商品詳細</router-link>
+      <router-link :to="{ name: 'product-review' }">レビュー</router-link>
+    </nav>
+    <!-- ここに子ルートを埋め込む -->
+    <router-view />
   </div>
-  <div v-else key="loading">商品情報を読み込んでいます...</div>
 </template>
 <script>
-import products from '@/api/products.js'
+import { mapGetters } from 'vuex'
 export default {
   props: { id: Number },
-  data() {
-    return {
-      item: null
-    }
-  },
+  computed: mapGetters('product', ['detail']),
   watch: {
     id: {
       handler() {
-        products.asyncFind(this.id, item => {
-          this.item = item
-        })
+        products.$store.dispatch('product/load', this.id)
       }, immediate: true
     }
+  },
+  beforeDestroy() {
+    // 親ルートを移動するとき商品詳細データを破棄
+    this.$store.dispatch('product/destroy')
   }
 }
 </script>
